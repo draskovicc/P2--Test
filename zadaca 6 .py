6a14c374bf50765d04d2f09e989257325b45de11
draskovicc <drazan.jukic@ff.sum.ba>
2026-06-20 03:38:09 +0200

    zadaca 6

---
+++ zadaca 6 .py
@@ -0,0 +1,20 @@
+imena = ["Miro", "Stjepan", "Josip", "Toni", "Robert", "Marko", "Ljubo", "Josipa", "Magdalena", "Ivana", "Stipe", "Emanuel", "Petar", "Ivan", "Luka", "Filip", "Lara", "Laura", "Mario", "Ana", "Marija", "Nikolina", "Andrija", "Slaven", "Sebastian", "Marko", "Frano", "Stipan", "Eugen", "Toni", "Dražan", "Matej", "Nives", "Nemanja", "Sara", "Ružica", "Gabrijel", "Darian", "Ivana", "Ivan Stjepan", "Kristian", "Josip", "Tomislav", "Jovan", "Gabrijel", "Mia", "Ante", "Josip", "Ante", "Josip", "Danijel", "Goran", "Zoran Ivan", "Franjo Francisko", "Sergej", "Matej", "Marin", "Sara", "Josip", "Stjepan", "Iva", "Marko", "Sara", "Krešimir", "Magdalena", "Marko", "Mirko", "David", "Ema", "Paul", "Sven", "Natalija", "Petar", "Emanuel", "Helena", "Antonio", "Petar"]
+
+brojac = {}
+for ime in imena:
+    if ime in brojac:
+        brojac[ime] += 1
+
+else:
+    brojac[ime] = 1
+
+
+print("ponavljanje imena:")
+for ime, broj in brojac.item():
+    print(ime, broj)
+
+najveci = max(brojac.value())
+print("najčešća imena:")
+for ime, broj in brojac.item()
+if broj == najveci:
+    print(ime, broj)
