4dbef20ad7e83a24770a435de30b88dd56de3645
draskovicc <drazan.jukic@ff.sum.ba>
2026-06-20 02:34:22 +0200

    zadaca 2

---
+++ zadaca 2 .py
@@ -0,0 +1,11 @@
+def ispisi_unatrag(drazan):
+    if drazan == "":
+        return
+    
+
+print(drazan)[-1], end="")
+ispisi_unatrag(drazan[:-1])
+
+unos = input("Unesi string: ")
+ispisi_unatrag(unos)
+
