8d712f20c5e4163858876bae94c40b92ddb4952a
draskovicc <drazan.jukic@ff.sum.ba>
2026-06-20 03:29:50 +0200

    zadaca 5

---
+++ zadaca 5 .py
@@ -0,0 +1,22 @@
+import random
+matrica = []
+
+for i in range(7):
+    red = []
+    for j in range(7):
+        red.append(random.randint(1,9))
+        matrica.append(red)
+
+
+        print("matrica:")
+        for red in matrica:
+            print(red)
+
+            zbroj = 00
+
+for i in range(7):
+    for j in range(7):
+        if i == 0 or i == 6 or j == 0 or j == 6
+        zbroj +== matrica (i)(j)
+
+print("zbroj elemenata na rubovima matrice:", zbroj)
